1f02d1d8c7f55e2258beea21b2c3fc40a2d2837a
apsmj23 <apurvalion@gmail.com>
2021-04-24 15:53:48 +0530

    Created Show Entry function

--- wiki_django/encyclopedia/views.py
+++ wiki_django/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . import util
-
+import markdown2
+from django.utils.html import format_html
 
 # Create your views here.
 
@@ -8,3 +9,27 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries" : util.list_entries()
     })
+
+
+def show_entry(request, title):
+    entry1 = util.list_entries()
+    for i in entry1:
+        if title == i:
+            try:
+                show_entry = format_html(markdown2.markdown(util.get_entry(title)))
+            except:
+                show_entry = ""
+            
+            
+    context = {
+        "title": title,
+        "entries": entry1
+    }
+    if not title in entry1:
+        return render(request, "encyclopedia/error.html", context)
+    
+    return render(request, "encyclopedia/entry.html", {
+       "entry" : show_entry,
+       "title" : title  
+    })
+
